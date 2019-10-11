bdc285a00a544b923a104005f2e1a95aa55f4ae2
Dan Finn <dan.finn@plansource.com>
2019-10-10 20:32:03 -0600

    fixing typos

--- character_pyramid.py
+++ character_pyramid.py
@@ -7,42 +7,21 @@
 pyramid_character = raw_input("Enter some text: ")
 pyramid_size = int(raw_input("And the size of your side word pyramid: "))
 
-
-<<<<<<< HEAD
-def create_pyramid(character,size):
-  """ Prints a sideword pyramid """
-  loop_counter = 1
-  increment = 1
-  while loop_counter > 0:
-   if increment:
-      print character * loop_counter
-      loop_counter += 1
-      if (loop_counter == size):
-        # Flip the increment counter and print the top of the pyramid
-        print character * loop_counter
-        increment = 0
-   else:
-     loop_counter -= 1
-     print character * loop_counter
-
-create_pyramid(pyramid_character,pyramid_size)
-=======
-def print_pyramid(pyramid_character, pyramid_size):
+def create_pyramid(character, size):
     """ Prints a sideword pyramid """
     loop_counter = 1
     increment = 1
     while loop_counter > 0:
         if increment:
-            print pyramid_character * loop_counter
+            print character * loop_counter
             loop_counter += 1
-            if (loop_counter == pyramid_size):
+            if (loop_counter == size):
                 # Flip the increment counter and print the top of the pyramid
-                print pyramid_character * loop_counter
+                print character * loop_counter
                 increment = 0
         else:
             loop_counter -= 1
-            print pyramid_character * loop_counter
+            print character * loop_counter
 
 
-print_pyramid(pyramid_character, pyramid_size)
->>>>>>> 8dacbdb3844b4d4091dfc58553a1031fda99b491
+create_pyramid(pyramid_character, pyramid_size)
