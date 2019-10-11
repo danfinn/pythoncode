79bccf807c2ac65b957c5a41f58feead2c38a39d
Dan Finn <dfinn@plansource.com>
2019-10-11 00:04:13 -0600

    much cleaner using range

--- character_pyramid.py
+++ character_pyramid.py
@@ -7,21 +7,13 @@
 pyramid_character = raw_input("Enter some text: ")
 pyramid_size = int(raw_input("And the size of your side word pyramid: "))
 
+
 def create_pyramid(character, size):
     """ Prints a sideword pyramid """
-    loop_counter = 1
-    increment = 1
-    while loop_counter > 0:
-        if increment:
-            print character * loop_counter
-            loop_counter += 1
-            if loop_counter == size:
-                # You've reached the top, start heading down the other side
-                print character * loop_counter
-                increment = 0
-        else:
-            loop_counter -= 1
-            print character * loop_counter
+    for i in range(1, size + 1):
+        print character * i
+    for i in range(size -1 , 1, -1):
+         print character * i
 
 
 create_pyramid(pyramid_character, pyramid_size)
